cc241a9fd9df30d43a3440099444936170466a56
Abdulaziz Ergashev <eabdulaziz36@gmail.com>
2024-07-02 03:23:07 +0500

    Array - For

---
+++ script.js
@@ -0,0 +1,113 @@
+// 5. shunday function yaratinki agument sifatida berilgan son array ichida bor yoki yo'qligini aniqlab bersin 
+// 6. shunday function yaratinki agument sifatida berilgan son array ichida bor bo'lsa array dan olib tashlab agar bo'lmasa array ning oxiridan qo'shib qo'ysin 
+
+
+
+
+
+// TASK1
+// 1. prompt da kiritilgan songacha bo'lgan juft va toq sonlar yig'indisini toping va ular o'rtasidagi farqni ham console ga chiqaring 
+
+
+// let number = +prompt("Son kiriting");
+
+// function doAction(number) {
+//     let evenSum = 0;
+//     let oddSum = 0;
+
+//     for (i = 0; i <= number; i++) {
+//         if (i % 2 == 0) {
+//             evenSum = evenSum + i;
+//         } else {
+//             oddSum = oddSum + i;
+//         }
+//     }
+//     console.log(evenSum);
+//     console.log(oddSum);
+// }
+
+// doAction(number);
+
+
+
+
+
+// TASK2
+// 2. shunday function yarating ki argument sifatida berilgan songacha bo'lgan sonlarning yig'indisi qaytarsin (return qilsin) 
+
+
+
+// function getSum(number) {
+//     let sum = 0;
+//     for (let i = 0; i <= number; i++) {
+//         sum += i; // (sum += i;) == (sum = sum + i;)
+//     }
+//     console.log(sum);
+// }
+// getSum(5);
+
+
+
+// function getSum(number) {
+//     let sum = 0;
+//     for (let i = 0; i <= number; i++) {
+//         sum += i; // (sum += i;) == (sum = sum + i;)
+//     }
+//     return sum;
+// }
+// console.log(getSum(5));
+
+
+
+
+// TASK3
+// 3. array ni ichidagi sonlar yig'indisini topib beradigan algorithm yarating array sonlardan tashkil topsin 
+
+
+// let numbers = [1, 10, 20, 30]
+// let sum = 0;
+// for ( let i = 0; i < numbers.length; i++) {
+//     sum = sum + numbers[i];
+// }
+// console.log(sum);
+
+
+
+
+
+
+
+// TASK4
+// 4. array ichidagi juft sonlar yig'indisini topadigan algorithm yarating 
+
+// let numbers = [1, 10, 20, 30]
+// let sum = 0;
+// for ( let i = 0; i < numbers.length; i++) {
+//    if (numbers[i] % 2 == 0){
+//     sum = sum + numbers[i];
+//    }
+// }
+// console.log(sum);
+
+
+
+
+// TASK5
+// TASK6
+
+
+
+
+// TASK7
+// 7. array ichidagi birinchi va oxirgi sonlar o'rtasidagi farqni toping farq har doim musbat son chiqishi kerak
+
+// let array = [1,2,3,4,5];
+// let firstElement = array[0];
+
+// let lastElement = array[array.length - 1];
+// let difference = Math.abs(firstElement - lastElement);
+
+// console.log(difference);
+
+
+
